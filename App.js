11c57ff0b09266c9be137f2a5147261644320df2
darshanmarviya <109666158+darshanmarviya@users.noreply.github.com>
2024-01-25 11:51:28 +0530

    App.js

--- App.js
+++ App.js
@@ -1,15 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import Idcard from './idcard';
+import Idcard2 from './idcard2';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{display:"flex"}}>
+
       <Idcard collname="Faculty of Science "
         name="Darshan Maraviya"
         rollno="33"
         Department="M.C.A"
-        Enrollmentnumber="230823066"/>
+        Enrollmentnumber="230651421"/>
+
+      <Idcard2 address = "Nana Mava Main Road, Near Kalavad Road, Rajkot-360005"
+              mobileno = "95756 36654"
+              email="atmiya.darsh.15618223062@gmail.com"
+              registration="15618223062"
+              bgroup="A+"/>
+    
     </div>
   );
 }

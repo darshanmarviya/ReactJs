a593f0b1cd3afc4d5376179f3bb0f32841af8d8f
darshanmarviya <109666158+darshanmarviya@users.noreply.github.com>
2024-01-25 09:32:01 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Idcard from './idcard';
+
+function App() {
+  return (
+    <div className="App">
+      <Idcard collname="Faculty of Science "
+        name="Darshan Maraviya"
+        rollno="33"
+        Department="M.C.A"
+        Enrollmentnumber="230823066"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ idcard.js
@@ -0,0 +1,27 @@
+import img from './images/man.jpg';
+import imga from './images/atmiya.png';
+import imgcode from './images/barcode.png';
+
+
+function Idcard(props)
+{
+    return(
+        <>
+            <div style={{width:"300px", height:"550px", border:"3px solid black", borderRadius:"10px", marginTop:"4%", marginLeft:"8%"}}>
+                <div style={{height:"18px", background:"orange", border:"0.2px solid white", borderRadius:"5px"}}></div>
+                <div style={{height:"18px", background:"green", border:"0.2px solid white", borderRadius:"5px"}}></div>
+                <img src={imga} height="80px"></img>
+                <p><b><u>{props.collname}</u></b></p>
+                <img src={img} height="82px"></img>
+                <p>{props.name}</p>
+                <p>{props.Department}</p>
+                <p>{props.rollno}</p>
+                <p>{props.Enrollmentnumber}</p>
+                <div style={{height:"15px", background:"orange", border:"0.2px solid white", borderRadius:"5px"}}></div>
+                <div style={{height:"15px", background:"green", border:"0.2px solid white", borderRadius:"5px"}}></div>
+                <img src={imgcode} height="100px"></img>
+            </div>
+        </>
+    );
+}
+export default Idcard;

6d6fd6adc2c32c7b2ab3e8fea28a33744fb0b048
darshanmarviya <109666158+darshanmarviya@users.noreply.github.com>
2024-01-29 11:01:25 +0530

    idcard.js

--- idcard.js
+++ idcard.js
@@ -1,16 +1,18 @@
 import img from './images/man.jpg';
 import imga from './images/atmiya.png';
 import imgcode from './images/barcode.png';
+//import back from './images/background.jpg';
+//import image from './images/background.jpg'; 
 
 
 function Idcard(props)
 {
     return(
         <>
-            <div style={{width:"300px", height:"550px", border:"3px solid black", borderRadius:"10px", marginTop:"4%", marginLeft:"8%"}}>
+            <div style={{width:"300px", height:"550px", border:"3px solid black", borderRadius:"10px", marginTop:"4%", marginLeft:"8%", /*backgroundImage:`url(${image})` */}}>
                 <div style={{height:"18px", background:"orange", border:"0.2px solid white", borderRadius:"5px"}}></div>
                 <div style={{height:"18px", background:"green", border:"0.2px solid white", borderRadius:"5px"}}></div>
-                <img src={imga} height="80px"></img>
+                <img src={imga} height="80px" style={{marginTop:"3px"}}></img>
                 <p><b><u>{props.collname}</u></b></p>
                 <img src={img} height="82px"></img>
                 <p>{props.name}</p>
@@ -25,4 +27,3 @@ function Idcard(props)
     );
 }
 export default Idcard;
-

7ee7f46f9880a5c2ecd0882f2eb923d2d6ff407b
darshanmarviya <109666158+darshanmarviya@users.noreply.github.com>
2024-01-31 10:49:16 +0530

    idcard.js

--- idcard.js
+++ idcard.js
@@ -3,15 +3,16 @@ import imga from './images/atmiya.png';
 import imgcode from './images/barcode.png';
 //import back from './images/background.jpg';
 //import image from './images/background.jpg'; 
+import background from './images/background.jpg';
 
 
 function Idcard(props)
 {
     return(
         <>
-            <div style={{width:"300px", height:"550px", border:"3px solid black", borderRadius:"10px", marginTop:"4%", marginLeft:"8%", /*backgroundImage:`url(${image})` */}}>
-                <div style={{height:"18px", background:"orange", border:"0.2px solid white", borderRadius:"5px"}}></div>
-                <div style={{height:"18px", background:"green", border:"0.2px solid white", borderRadius:"5px"}}></div>
+            <div style={{width:"300px", height:"550px", border:"3px solid black", borderRadius:"10px", marginTop:"4%", marginLeft:"8%", backgroundImage:`url(${background})`,backgroundRepeat: "no-repeat",backgroundSize: "cover"}}>
+                <div style={{height:"18px", background:"#3f4dcc", border:"0.2px solid white", borderRadius:"5px"}}></div>
+                <div style={{height:"18px", background:"#cc37bd", border:"0.2px solid white", borderRadius:"5px"}}></div>
                 <img src={imga} height="80px" style={{marginTop:"3px"}}></img>
                 <p><b><u>{props.collname}</u></b></p>
                 <img src={img} height="82px"></img>
@@ -19,8 +20,8 @@ function Idcard(props)
                 <p>{props.Department}</p>
                 <p>{props.rollno}</p>
                 <p>{props.Enrollmentnumber}</p>
-                <div style={{height:"15px", background:"orange", border:"0.2px solid white", borderRadius:"5px"}}></div>
-                <div style={{height:"15px", background:"green", border:"0.2px solid white", borderRadius:"5px"}}></div>
+                <div style={{height:"15px", background:"#3f4dcc", border:"0.2px solid white", borderRadius:"5px"}}></div>
+                <div style={{height:"15px", background:"#cc37bd", border:"0.2px solid white", borderRadius:"5px"}}></div>
                 <img src={imgcode} height="100px"></img>
             </div>
         </>

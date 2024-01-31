23b4515966c2fd9c778994be9f491586f61f0ead
darshanmarviya <109666158+darshanmarviya@users.noreply.github.com>
2024-01-31 11:30:44 +0530

    idcard.js

--- idcard.js
+++ idcard.js
@@ -14,15 +14,35 @@ function Idcard(props)
                 <div style={{height:"18px", background:"#3f4dcc", border:"0.2px solid white", borderRadius:"5px"}}></div>
                 <div style={{height:"18px", background:"#cc37bd", border:"0.2px solid white", borderRadius:"5px"}}></div>
                 <img src={imga} height="80px" style={{marginTop:"3px"}}></img>
-                <p><b><u>{props.collname}</u></b></p>
+
+                <div style={{marginTop:"15px"}}>
+                <p style={{marginLeft:"5px", fontSize:"14px", marginTop:"18px"}}><b><u>{props.collname}</u></b></p>
+                </div>
+
                 <img src={img} height="82px"></img>
-                <p>{props.name}</p>
-                <p>{props.Department}</p>
-                <p>{props.rollno}</p>
-                <p>{props.Enrollmentnumber}</p>
+
+                <div style={{marginTop:"-10px"}}>
+                <p style={{marginLeft:"5px", fontSize:"14px", marginTop:"18px"}}><b>{props.name}</b></p>
+                </div>
+
+                <div style={{marginTop:"-10px"}}>
+                <p style={{marginLeft:"5px", fontSize:"14px", marginTop:"18px"}}><b>{props.rollno}</b></p>
+                </div>
+
+                <div style={{marginTop:"-10px"}}>
+                <p style={{marginLeft:"5px", fontSize:"14px", marginTop:"18px"}}><b>{props.Department}</b></p>
+                </div>
+
+                <div style={{marginTop:"-10px"}}>
+                <p style={{marginLeft:"5px", fontSize:"14px", marginTop:"18px"}}><b>{props.year}</b></p>
+                </div>
+
+                <div style={{height:"5px", background:"#3f4dcc", border:"0.2px solid white", borderRadius:"5px", marginTop:"37px"}}></div>
+
+                <img src={imgcode} height="100px"></img>
+                
                 <div style={{height:"15px", background:"#3f4dcc", border:"0.2px solid white", borderRadius:"5px"}}></div>
                 <div style={{height:"15px", background:"#cc37bd", border:"0.2px solid white", borderRadius:"5px"}}></div>
-                <img src={imgcode} height="100px"></img>
             </div>
         </>
     );

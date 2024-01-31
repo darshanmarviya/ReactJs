4ce0898d111290484efc594f6a49b50807a1a980
darshanmarviya <109666158+darshanmarviya@users.noreply.github.com>
2024-01-31 11:31:09 +0530

    idcard2.js

--- idcard2.js
+++ idcard2.js
@@ -13,38 +13,56 @@ function Idcard2(props)
                 <div style={{height:"18px", background:"#cc37bd", border:"0.2px solid white", borderRadius:"5px"}}></div>
                 <img src={imga} height="80px" style={{marginTop:"3px"}}></img>
 
-                <div style={{display:"flex"}}>
+                <div style={{display:"flex",marginTop:"8px"}}>
                 <p style={{marginLeft:"5px"}}><b><u>Address</u></b></p>
                 <p style={{marginLeft:"15px"}}><b>:</b></p> 
                 <p style={{marginLeft:"5px", fontSize:"14px", marginTop:"18px"}}>{props.address}</p>
                 </div>
                 
-                <div style={{display:"flex"}}>
+                <div style={{display:"flex",marginTop:"-17px"}}>
                 <p style={{marginLeft:"5px"}}><b><u>Mobile Number</u></b></p> 
                 <p style={{marginLeft:"5px"}}><b>:</b></p> 
                 <p style={{marginLeft:"5px"}}>{props.mobileno}</p>
                 </div>
 
-                <div style={{display:"flex"}}>
-                <p style={{marginLeft:"5px"}}><b><u>Email</u></b></p>
+
+                <div style={{display:"flex",marginTop:"-17px"}}>
+                <p style={{marginLeft:"5px"}}><b><u>Aadhar Card</u></b></p>
                 <p style={{marginLeft:"5px"}}><b>:</b></p> 
-                <p style={{marginLeft:"5px", marginTop:"17px"}}>{props.email}</p>
+                <p style={{marginLeft:"5px"}}>{props.aadhar}</p>
                 </div>
 
-                <div style={{display:"flex"}}>
-                <p style={{marginLeft:"5px"}}><b><u>Registration Number</u></b></p>
-                <p style={{marginLeft:"5px"}}><b>:</b></p> 
-                <p style={{marginLeft:"5px"}}>{props.registration}</p>
-                </div>
-
-                <div style={{display:"flex"}}>
+                <div style={{display:"flex",marginTop:"-17px"}}>
                 <p style={{marginLeft:"5px"}}><b><u>Blood Group</u></b></p>
                 <p style={{marginLeft:"5px"}}><b>:</b></p> 
                 <p style={{marginLeft:"5px"}}>{props.bgroup}</p>
                 </div>
 
+                <div style={{display:"flex",marginTop:"-17px"}}>
+                <p style={{marginLeft:"5px"}}><b><u>Enrollment No</u></b></p>
+                <p style={{marginLeft:"5px"}}><b>:</b></p> 
+                <p style={{marginLeft:"5px"}}>{props.enrollment}</p>
+                </div>
+
+                <div style={{display:"flex",marginTop:"-17px"}}>
+                <p style={{marginLeft:"5px"}}><b><u>Date of Birth</u></b></p>
+                <p style={{marginLeft:"5px"}}><b>:</b></p> 
+                <p style={{marginLeft:"5px"}}>{props.dob}</p>
+                </div>
+
+                <div style={{fontSize:"12px"}}>
+                <p>ID Card is compulsory on Campus</p>
+                </div>
+
+                <div style={{height:"5px", background:"#3f4dcc", border:"0.2px solid white", borderRadius:"5px"}}></div>
+                <div style={{display:"flex"}}>
+                <p style={{marginLeft:"5px",fontSize:"14px"}}><b>{props.location}</b></p>
+                </div>
+
                 <div style={{height:"15px", background:"#3f4dcc", border:"0.2px solid white", borderRadius:"5px"}}></div>
                 <div style={{height:"15px", background:"#cc37bd", border:"0.2px solid white", borderRadius:"5px"}}></div>
+
+
             </div>
         </>
 
